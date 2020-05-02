a2154a6a0a23d11f5af874909bc351003ecef718
unknown <1269158832@qq.com>
2020-05-02 13:52:42 +0800

    [Fix] PHP7.4 Support Problem

--- data/forum/api/live2d/rand_textures/index.php
+++ data/forum/api/live2d/rand_textures/index.php
@@ -16,15 +16,19 @@ $modelTexturesId = isset($id[1]) ? (int)$id[1] : false;
 $modelName = $modelList->id_to_name($modelId);
 $modelTexturesList = is_array($modelName) ? array('textures' => $modelName) : $modelTextures->get_list($modelName);
 
-if (count($modelTexturesList['textures']) <= 1) {
-    $modelTexturesNewId = 1;
-} else {
-    $modelTexturesGenNewId = true;
-    if ($modelTexturesId == 0) $modelTexturesId = 1;
-    while ($modelTexturesGenNewId) {
-        $modelTexturesNewId = rand(0, count($modelTexturesList['textures'])-1)+1;
-        $modelTexturesGenNewId = $modelTexturesNewId == $modelTexturesId ? true : false;
+if (is_array($modelTexturesList['textures'])) {
+    if (count($modelTexturesList['textures']) <= 1) {
+        $modelTexturesNewId = 1;
+    } else {
+        $modelTexturesGenNewId = true;
+        if ($modelTexturesId == 0) $modelTexturesId = 1;
+        while ($modelTexturesGenNewId) {
+            $modelTexturesNewId = rand(0, count($modelTexturesList['textures'])-1)+1;
+            $modelTexturesGenNewId = $modelTexturesNewId == $modelTexturesId ? true : false;
+        }
     }
+} else {
+	$modelTexturesNewId = 1;
 }
 
 header("Content-type: application/json");
