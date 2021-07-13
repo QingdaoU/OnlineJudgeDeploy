4645374607d06cdad3960a11c3028ce98792d76a
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-07-12 23:11:37 -0500

    Create Macros.rs

---
+++ backup/Macros.rs
@@ -0,0 +1,9 @@
+Crate uniffi_macrosCopy item import
+[−][src]
+Macros for uniffi.
+
+Currently this is just for easily generating integration tests, but maybe we’ll put some other code-annotation helper macros in here at some point.
+
+Macros
+build_foreign_language_testcases	A macro to build testcases for a component’s generated bindings.
+include_scaffolding	A helper macro to include generated component scaffolding.
