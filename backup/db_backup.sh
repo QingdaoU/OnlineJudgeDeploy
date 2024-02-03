25d623ef924708c5aeab8a332ea6b8047ac7d315
Mikucat <admin@mikucat.me>
2024-02-03 23:58:37 +0800

    feat: v1.6.0

--- backup/db_backup.sh
+++ backup/db_backup.sh
@@ -1,1 +1,1 @@
-docker exec -it oj-postgres pg_dumpall -c -U onlinejudge > db_backup_`date +%Y_%m_%d"_"%H_%M_%S`.sql
+docker compose exec -it oj-postgres pg_dumpall -c -U onlinejudge > db_backup_`date +%Y_%m_%d"_"%H_%M_%S`.sql
